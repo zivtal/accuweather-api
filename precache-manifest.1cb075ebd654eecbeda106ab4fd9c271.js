4aa730d4c3388384e423a07d47d248e1b87ef234
zivtal <zivtal83@gmail.com>
2022-03-24 10:59:09 +0200

    New Deployment

---
+++ precache-manifest.1cb075ebd654eecbeda106ab4fd9c271.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1182396b86f6527585d4",
+    "url": "/accuweather-api/css/app.7d54eddb.css"
+  },
+  {
+    "revision": "e9192123f6dc8738042aa79a6c18f689",
+    "url": "/accuweather-api/img/celsius.e9192123.svg"
+  },
+  {
+    "revision": "bdc124f0732682c586679c4ae8f04e82",
+    "url": "/accuweather-api/img/dark-mode.bdc124f0.svg"
+  },
+  {
+    "revision": "0cb0b2ff6c353aa266f114a1b0c691d8",
+    "url": "/accuweather-api/img/fahrenheit.0cb0b2ff.svg"
+  },
+  {
+    "revision": "d1d08460bfd28080eb4d570f6baeaa60",
+    "url": "/accuweather-api/img/favorite.d1d08460.svg"
+  },
+  {
+    "revision": "3fc1fd6dc7c69aa491a33a9bca8d8f2d",
+    "url": "/accuweather-api/img/home.3fc1fd6d.svg"
+  },
+  {
+    "revision": "0b888b6feab76095832e24317e9b3bc9",
+    "url": "/accuweather-api/img/ilike.0b888b6f.svg"
+  },
+  {
+    "revision": "bedd11732fd5a0181b1d6686a17bc4b4",
+    "url": "/accuweather-api/img/like.bedd1173.svg"
+  },
+  {
+    "revision": "fe3623edfe782549a42ca97000de12ab",
+    "url": "/accuweather-api/img/location.fe3623ed.svg"
+  },
+  {
+    "revision": "0e7feb70f12f36a9cdce2e0033fc742a",
+    "url": "/accuweather-api/img/search.0e7feb70.svg"
+  },
+  {
+    "revision": "5e7827a1cc16cfc3912beb9032e7907f",
+    "url": "/accuweather-api/index.html"
+  },
+  {
+    "revision": "13c13b22200f68acc46e",
+    "url": "/accuweather-api/js/about.e1a2e4d7.js"
+  },
+  {
+    "revision": "1182396b86f6527585d4",
+    "url": "/accuweather-api/js/app.81d89775.js"
+  },
+  {
+    "revision": "eb4e89752398760d3672",
+    "url": "/accuweather-api/js/chunk-vendors.fc25ea7c.js"
+  },
+  {
+    "revision": "8408cbf79237f5508a03549f30fae7f1",
+    "url": "/accuweather-api/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/accuweather-api/robots.txt"
+  }
+]);
